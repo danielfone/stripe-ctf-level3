9432eb5c17b65a4c24749449cf57c4946e01c5e7
Daniel Fone <daniel@fone.net.nz>
2014-01-25 20:43:31 +1300

    Basically functioning app

--- search.rb
+++ search.rb
@@ -3,3 +3,24 @@ require 'sinatra'
 get '/healthcheck' do
   '{"success": "true"}'
 end
+
+get '/index' do
+  puts "-->>> Would index #{params[:path]}"
+end
+
+get '/isIndexed' do
+  '{"success": "true"}'
+end
+
+
+get '/' do
+  <<-JSON
+  {
+    "success": true,
+    "results": [
+      "path/to/file1:5",
+      "path/to/another/file:33"
+    ]
+  }
+  JSON
+end
