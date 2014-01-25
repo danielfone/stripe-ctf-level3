b1b390ecdbd96e8adf04f3eca91b9b41d040e6c6
Daniel Fone <daniel@fone.net.nz>
2014-01-25 21:28:08 +1300

    LOL grep

--- search.rb
+++ search.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 
+configure do
+  set :index_path, 'test/data/input/'
+end
+
+
+@path = 
+
 get '/healthcheck' do
   '{"success": "true"}'
 end
 
 get '/index' do
-  puts "-->>> Would index #{params[:path]}"
+  @path = params[:path]
 end
 
 get '/isIndexed' do
@@ -14,13 +21,16 @@ end
 
 
 get '/' do
+  q = params[:q]
+  return '{"success": "false"}' if q.nil?
+
+  cmd = %Q[cd #{settings.index_path}; grep '#{params[:q]}' -Rn . | cut -d ':' -f 1,2 | cut -d '/' -f 2-]
+  results = `#{cmd}`.strip
+  rjson = results.split("\n")
   <<-JSON
   {
     "success": true,
-    "results": [
-      "path/to/file1:5",
-      "path/to/another/file:33"
-    ]
+    "results": #{rjson.inspect}
   }
   JSON
 end
